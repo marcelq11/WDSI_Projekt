119a3c1d62675baacb1c473417c2f5efb1b3d66c
marcelq11 <marcelq38@gmail.com>
2022-02-06 15:06:48 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+
+
+import os
+import random
+import numpy as np
+import cv2
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+import pandas
+
+def main():
+
+    return
+
+
+if __name__ == '__main__':
+    main()
