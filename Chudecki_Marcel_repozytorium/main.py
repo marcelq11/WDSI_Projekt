a4bb3eda01a15b20509e283071a2548c3a9faa16
marcelq11 <marcelq38@gmail.com>
2022-02-15 22:51:22 +0100

    15.02(2)

--- Chudecki_Marcel_repozytorium/main.py
+++ Chudecki_Marcel_repozytorium/main.py
@@ -10,34 +10,73 @@ import pandas
 import xml.etree.ElementTree as ET
 from pathlib import Path
 
-def load_data(path,train_quantity):
+def load_data(path):
     data = []
-    for i in range(train_quantity):
-        tree = ET.parse(path+'annotations\\road'+str(i)+'.xml')
+    for i in os.listdir(path+'annotations/'):
+        tree = ET.parse(path+'annotations/'+i)
         filename = tree.find('filename')
-        print(filename.text)
+        # print(filename.text)
         objects = tree.findall('object')
-        print(len(objects))
+        # print(len(objects))
         for i in objects:
             x_min = i.find('bndbox/xmin')
             y_min = i.find('bndbox/ymin')
             x_max = i.find('bndbox/xmax')
             y_max = i.find('bndbox/ymax')
-            print(x_min.text,y_min.text,x_max.text,y_max.text)
-            image = cv2.imread(path+'images\\'+filename.text)
+            # print(x_min.text,y_min.text,x_max.text,y_max.text)
+            image = cv2.imread(path+'images/'+filename.text)
             image_cropped = image[int(y_min.text):int(y_max.text), int(x_min.text):int(x_max.text)]
-            data.append({'image': image_cropped, 'label': i.find('name').text})
+            if str(i.find('name').text) == 'speedlimit':
+                data.append({'image': image_cropped, 'label': str(i.find('name').text)})
+            else:
+                data.append({'image': image_cropped, 'label': 'other'})
+    return data
+
+def learn_bovw(data):
+    dict_size = 128
+    bow = cv2.BOWKMeansTrainer(dict_size)
+
+    sift = cv2.SIFT_create()
+    for sample in data:
+        kpts = sift.detect(sample['image'], None)
+        kpts, desc = sift.compute(sample['image'], kpts)
+
+        if desc is not None:
+            bow.add(desc)
+
+    vocabulary = bow.cluster()
+
+    np.save('voc.npy', vocabulary)
+
+def extract_features(data):
+    sift = cv2.SIFT_create()
+    flann = cv2.FlannBasedMatcher_create()
+    bow = cv2.BOWImgDescriptorExtractor(sift, flann)
+    vocabulary = np.load('voc.npy')
+    bow.setVocabulary(vocabulary)
+
+    for sample in data:
+        kpts = sift.detect(sample['image'], None)
+        imgDes = bow.compute(sample['image'], kpts)
+        if imgDes is not None:
+            sample.update({'desc': imgDes})
+        else:
+            sample.update({'desc': np.zeros((1, 128))})
     return data
 
 def main():
-    train_quantity = 5
     main = Path('main.py')
     main_path = main.parent.absolute()
     absolute_path=main_path.parent
-    data = load_data(str(absolute_path)+'\\train\\',train_quantity)
-    for i in range(len(data)):
-        cv2.imshow(data[i]['label'],data[i]['image'])
-        cv2.waitKey(0)
+    data_train = load_data(str(absolute_path) + '/train/')
+    data_test = load_data(str(absolute_path) + '/test/')
+    print(len(data_train))
+    print(len(data_test))
+    # learn_bovw(data)
+    # data = extract_features(data)
+    # for i in range(len(data)):
+    #     cv2.imshow(data[i]['label'],data[i]['image'])
+    #     cv2.waitKey(0)
     # img = cv2.imread('train/images/road0.png')
     # cv2.rectangle(img,(98,62),(208,232),(0,255,0))
     # cv2.imshow('test',img)
